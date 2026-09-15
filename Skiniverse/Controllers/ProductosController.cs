44d4acca6a6905450c2bd25e332649de2cfc30a2
patariavanesa61 <patariavanesa61@gmail.com>
2026-09-15 08:38:30 -0500

    Vanesa Pataria

--- Skiniverse/Controllers/ProductosController.cs
+++ Skiniverse/Controllers/ProductosController.cs
@@ -14,6 +14,47 @@ namespace Skiniverse.Controllers
             _configuration = configuration;
         }
 
+        // GET: /Productos/Detalles/5
+        public IActionResult Detalles(int id)
+        {
+            Producto producto = null;
+            string conexionString = _configuration.GetConnectionString("ConexionSQL");
+
+            using (SqlConnection conexion = new SqlConnection(conexionString))
+            {
+                string query = "SELECT IdProducto, NombreProducto, Categoria, TipoPielRecomendado, IngredientesActivos, PrecioRegular, StockActual, ImagenUrl FROM Productos WHERE IdProducto = @Id";
+                SqlCommand cmd = new SqlCommand(query, conexion);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                conexion.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        producto = new Producto
+                        {
+                            IdProducto = Convert.ToInt32(reader["IdProducto"]),
+                            NombreProducto = reader["NombreProducto"].ToString() ?? "",
+                            Categoria = reader["Categoria"].ToString() ?? "",
+                            TipoPielRecomendado = reader["TipoPielRecomendado"].ToString() ?? "",
+                            IngredientesActivos = reader["IngredientesActivos"].ToString() ?? "",
+                            PrecioRegular = Convert.ToDecimal(reader["PrecioRegular"]),
+                            StockActual = Convert.ToInt32(reader["StockActual"]),
+                            ImagenUrl = reader["ImagenUrl"] != DBNull.Value ? reader["ImagenUrl"].ToString() : ""
+                        };
+                    }
+                }
+            }
+
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
         // GET: /Productos/Catalogo
         public IActionResult Catalogo()
         {
